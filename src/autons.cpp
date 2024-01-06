91a50ed0c84e45308a5bb6b97368c5d67566c3fe
Larfy1 <aaronyu2019@gmail.com>
2024-01-05 19:49:08 -0500

    tuning

--- src/autons.cpp
+++ src/autons.cpp
@@ -26,9 +26,9 @@ const int SWING_SPEED = 90;
 void default_constants() {
   chassis.set_slew_min_power(80, 80);
   chassis.set_slew_distance(7, 7);
-  chassis.set_pid_constants(&chassis.headingPID, 11, 0, 20, 0);
-  chassis.set_pid_constants(&chassis.forward_drivePID, 0.45, 0, 5, 0);
-  chassis.set_pid_constants(&chassis.backward_drivePID, 0.45, 0, 5, 0);
+  chassis.set_pid_constants(&chassis.headingPID, 13, 0, 55, 0);
+  chassis.set_pid_constants(&chassis.forward_drivePID, 0.45, 0, 6, 0);
+  chassis.set_pid_constants(&chassis.backward_drivePID, 0.5, 0, 3, 0);
   chassis.set_pid_constants(&chassis.turnPID, 5, 0.003, 35, 15);
   chassis.set_pid_constants(&chassis.swingPID, 7, 0, 45, 0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,49 +3,32 @@
 
 // Chassis constructor
 Drive chassis (
-  // Left Chassis Ports (negative port will reverse it!)
-  //   the first port is the sensored port (when trackers are not used!)
+  // Left Chassis Ports
   {-15, -16, -17}
-
-  // Right Chassis Ports (negative port will reverse it!)
-  //   the first port is the sensored port (when trackers are not used!)
+  // Right Chassis Ports
   ,{11, 12, 14}
-
   // IMU Port
   ,18
-
-  // Wheel Diameter (Remember, 4" wheels are actually 4.125!)
-  //    (or tracking wheel diameter)
+  // Wheel Diameter
   ,3.25
-
   // Cartridge RPM
-  //   (or tick per rotation if using tracking wheels)
   ,600
-
   // External Gear Ratio (MUST BE DECIMAL)
-  //    (or gear ratio of tracking wheel)
-  // eg. if your drive is 84:36 where the 36t is powered, your RATIO would be 2.333.
-  // eg. if your drive is 36:60 where the 60t is powered, your RATIO would be 0.6.
-  ,0.75
-
-
-  // Uncomment if using tracking wheels
-  /*
-  // Left Tracking Wheel Ports (negative port will reverse it!)
-  // ,{1, 2} // 3 wire encoder
-  // ,8 // Rotation sensor
-
-  // Right Tracking Wheel Ports (negative port will reverse it!)
-  // ,{-3, -4} // 3 wire encoder
-  // ,-9 // Rotation sensor
-  */
-
-  // Uncomment if tracking wheels are plugged into a 3 wire expander
-  // 3 Wire Port Expander Smart Port
-  // ,1
+  ,1.333
 );
 
+pros::ADIDigitalOut wings('a');
+pros::ADIDigitalOut pistonIntake('b');
+pros::ADIDigitalOut arm('c');
+pros::ADIDigitalOut blocker('d');
 
+pros::ADIDigitalIn upSelect('e');
+pros::ADIDigitalIn done('f');
+
+pros::Motor intakeMotor(14);
+
+pros::Motor cataMotor(13);
+pros::Rotation cataRot(1);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -71,11 +54,11 @@ void initialize() {
 
   // Autonomous Selector using LLEMU
   ez::as::auton_selector.add_autons({
-    Auton("Example Drive\n\nDrive forward and come back.", drive_example),
+    Auton("Swing Example\n\nSwing, drive, swing.", swing_example),
     Auton("Example Turn\n\nTurn 3 times.", turn_example),
+    Auton("Example Drive\n\nDrive forward and come back.", drive_example),
     Auton("Drive and Turn\n\nDrive forward, turn, come back. ", drive_and_turn),
     Auton("Drive and Turn\n\nSlow down during drive.", wait_until_change_speed),
-    Auton("Swing Example\n\nSwing, drive, swing.", swing_example),
     Auton("Combine all 3 movements", combining_movements),
     Auton("Interference\n\nAfter driving forward, robot performs differently if interfered or not.", interfered_example),
   });
